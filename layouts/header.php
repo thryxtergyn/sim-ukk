fc8ce37ea9ffa60dc81b6b674b76d2858c04d593
DzakiNur <dzakinurmuhammadaflahsuherman@smkwikrama.sch.id>
2023-02-23 15:24:14 +0700

    initial commit

---
+++ layouts/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width-device-width, initial-scale=1">
+    <title>Aplikasi Pengaduan Masyarakat | Ginara</title>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+</head>
+<body>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.php">Aplikasi Pengaduan Masyarakat</a>
+       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+       </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?page=registrasi">Daftar Akun</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php?page=login">Login</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
